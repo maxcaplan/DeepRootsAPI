ea69f7b8d976a45a1987cf6a0ff2d9cd84b435b6
Max <maxacaplan@gmail.com>
2021-03-30 11:36:08 -0300

    express boilerplate added

---
+++ app.js
@@ -0,0 +1,14 @@
+var argv = require("minimist")(process.argv.slice(2));
+
+const express = require("express");
+
+const app = express();
+const port = argv.Port || argv.port || argv.P || argv.p || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
